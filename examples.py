6b89ac5fd9ad624047a110d90d63f7fca6419063
Mike Trenfield <m.trenfield@gmail.com>
2015-09-10 22:34:29 -0500

    Added Hillary Clinton's announcement speech.

--- examples.py
+++ examples.py
@@ -36,14 +36,27 @@ def load_huckabee():
     text = filter(lambda x: x != '', text)
     return tokenize('\n'.join(text))
 
+def load_clinton():
+    f = open('example_text'+os.sep+'clinton.txt')
+    text = map(lambda x: x.rstrip(), f.readlines())
+    f.close()
+    text = filter(lambda x: not 'APPLAUSE' in x, text)
+    text = filter(lambda x: not x.startswith('AUDIENCE'), text)
+    text = map(lambda x: x.replace("\"", ''), text)
+    text = filter(lambda x: x != '', text)
+    return tokenize('\n'.join(text))
+
 if __name__ == '__main__':
     cruz = load_cruz()
     trump = load_trump()
     huckabee = load_huckabee()
+    clinton = load_clinton()
     print
     print "Ted Cruz: ", gen_sentence(cruz)
     print
     print "Donald Trump: ", gen_sentence(trump)
     print
     print "Mike Huckabee: ", gen_sentence(huckabee)
+    print
+    print "Hillary Clinton: ", gen_sentence(clinton)
     print
